ea93b30022b5c78701974bba2a5dd5b740e6a1ed
LeticiaSFranca <leticiasilvafranca17@gmail.com>
2024-01-02 22:31:21 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -3,38 +3,34 @@ window.addEventListener("scroll", function(){
   header.classList.toggle('rolagem', window.scrollY > 0)
 })
 // -----------------------------------------------------------------------
-function mascara_telefone() {
-  //limitador
-  var tel = document.getElementById("telefone").value
+
+function tel_mask() {
+  var tel = document.getElementById("tel").value
   console.log(tel)
-  tel = tel.slice(0, 14) //(pode limitar a quantidade de char na entrada pelo java script)
+  tel = tel.slice(0,14)
   console.log(tel)
-  document.getElementById("telefone").value = tel
-  tel = document.getElementById("telefone").value.slice(0, 10)
+  document.getElementById("tel").value=tel
+  tel = document.getElementById("tel").value.slice(0,10)
   console.log(tel)
 
-  //máscara
-  var tel_formatado = document.getElementById("telefone").value
-  if (tel_formatado[0] != "(") {
-    if (tel_formatado[0] != undefined) {
-      document.getElementById("telefone").value = "(" + tel_formatado[0];
-    }
+  var formatted_tel = document.getElementById("tel").value
+  if (formatted_tel[0] != "("){
+      if(formatted_tel[0] != undefined){
+          document.getElementById("tel").value = "(" + formatted_tel[0];
+      }
   }
 
-  if (tel_formatado[3] != ")") {
-    if (tel_formatado[3] != undefined) {
-      document.getElementById("telefone").value = tel_formatado.slice(0, 3) + ")" + tel_formatado[3]
-    }
+  if (formatted_tel[3] != ")"){
+      if(formatted_tel[3] != undefined){
+          document.getElementById("tel").value = formatted_tel.slice(0,3) + ")" + formatted_tel[3]
+      }
   }
 
-  if (tel_formatado[9] != "-") {
-    if (tel_formatado[9] != undefined) {
-      document.getElementById("telefone").value = tel_formatado.slice(0, 9) + "-" + tel_formatado[9]
-    }
+  if (formatted_tel[9]!="-"){
+      if(formatted_tel[9] != undefined){
+          document.getElementById("tel").value = formatted_tel.slice(0,9)+"-" + formatted_tel[9]
+      }
   }
 }
 
 // -----------------------------------------------------------------------
-
-// Validação de Formulário
-const form = document.getElementById("form")
